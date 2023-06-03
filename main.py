3212ba7f1ee1b189d4b92f0475933398b78e8b81
Susmitha <susmithareddym4@gamil.com>
2023-06-03 18:21:54 +0530

    made change for second time

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
-df=pd.read_csv(r"D:\MyProject\matches.csv")
+df=pd.read_csv("D:\\MyProject\\matches.csv")
 print(df.head())
 print(df.info())
 print(df.summary())
-print(df.describe())
-print(df.values())
 print(df)
